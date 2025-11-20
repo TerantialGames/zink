8221e16bff8a8ec2663ca2c2d298ac33f8ad3618
TerantialGames <teranitaldevs@gmail.com>
2025-11-20 14:42:13 -0500

    Add signup form with email, username, and password

---
+++ signup.js
@@ -0,0 +1,6 @@
+<input id="email" class="input" type="email" placeholder="Email" required />
+<input id="username" class="input" type="text" placeholder="Username" required />
+<input id="password" class="input" type="password" placeholder="Password" required />
+<input id="confirm" class="input" type="password" placeholder="Confirm Password" required />
+
+<button class="submit-btn" onclick="signUp()">Create Account</button>
